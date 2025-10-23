64e4558d9070e0a5b0b140a87243c3a0c4013e17
LeonardoMattosMartins <leom@bu.edu>
2025-10-23 00:33:09 -0400

    https://www.youtube.com/watch?v=qnkuBUAwfe0&list=RDqnkuBUAwfe0&start_radio=1

---
+++ Demo/CM7/Core/Src/main.c
@@ -0,0 +1,477 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "math.h" // You MUST include math.h for fabs()
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* DUAL_CORE_BOOT_SYNC_SEQUENCE: Define for dual core boot synchronization    */
+/*                             demonstration code based on hardware semaphore */
+/* This define is present in both CM7/CM4 projects                            */
+/* To comment when developping/debugging on a single core                     */
+#define DUAL_CORE_BOOT_SYNC_SEQUENCE
+
+#if defined(DUAL_CORE_BOOT_SYNC_SEQUENCE)
+#ifndef HSEM_ID_0
+#define HSEM_ID_0 (0U) /* HW semaphore 0*/
+#endif
+#endif /* DUAL_CORE_BOOT_SYNC_SEQUENCE */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+COM_InitTypeDef BspCOMInit;
+
+TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim3;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_TIM3_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+#define DEGREES(raw_count) ((double)((raw_count) % 1203) * (360.0f / 1203.0f))
+
+volatile uint16_t counter = 0;
+// --- PID Tuning Constants (MUST BE TUNED!) ---
+#define KP 2.5f                // Proportional gain
+#define KI 0.1f                // Integral gain
+#define KD 0.05f                // Derivative gain
+#define TARGET_ANGLE 0.0f    // Your desired setpoint (e.g., 180 degrees)
+
+// --- PID Limits ---
+#define MAX_PWM 300.0f         // Max PWM value (from your 300)
+#define MAX_INTEGRAL 180.0f    // Anti-windup limit (tune this)
+//void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+//	counter = __HAL_TIM_GET_COUNTER(htim);
+//}
+
+/* USER CODE END 0 */
+
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+
+	/* USER CODE BEGIN 1 */
+
+	/* USER CODE END 1 */
+	/* USER CODE BEGIN Boot_Mode_Sequence_0 */
+#if defined(DUAL_CORE_BOOT_SYNC_SEQUENCE)
+	int32_t timeout;
+#endif /* DUAL_CORE_BOOT_SYNC_SEQUENCE */
+	/* USER CODE END Boot_Mode_Sequence_0 */
+
+	/* USER CODE BEGIN Boot_Mode_Sequence_1 */
+#if defined(DUAL_CORE_BOOT_SYNC_SEQUENCE)
+	/* Wait until CPU2 boots and enters in stop mode or timeout*/
+	timeout = 0xFFFF;
+	while ((__HAL_RCC_GET_FLAG(RCC_FLAG_D2CKRDY) != RESET) && (timeout-- > 0))
+		;
+	if (timeout < 0) {
+		Error_Handler();
+	}
+#endif /* DUAL_CORE_BOOT_SYNC_SEQUENCE */
+	/* USER CODE END Boot_Mode_Sequence_1 */
+	/* MCU Configuration--------------------------------------------------------*/
+
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
+
+	/* USER CODE BEGIN Init */
+
+	/* USER CODE END Init */
+
+	/* Configure the system clock */
+	SystemClock_Config();
+	/* USER CODE BEGIN Boot_Mode_Sequence_2 */
+#if defined(DUAL_CORE_BOOT_SYNC_SEQUENCE)
+	/* When system initialization is finished, Cortex-M7 will release Cortex-M4 by means of
+	 HSEM notification */
+	/*HW semaphore Clock enable*/
+	__HAL_RCC_HSEM_CLK_ENABLE();
+	/*Take HSEM */
+	HAL_HSEM_FastTake(HSEM_ID_0);
+	/*Release HSEM in order to notify the CPU2(CM4)*/
+	HAL_HSEM_Release(HSEM_ID_0, 0);
+	/* wait until CPU2 wakes up from stop mode */
+	timeout = 0xFFFF;
+	while ((__HAL_RCC_GET_FLAG(RCC_FLAG_D2CKRDY) == RESET) && (timeout-- > 0))
+		;
+	if (timeout < 0) {
+		Error_Handler();
+	}
+#endif /* DUAL_CORE_BOOT_SYNC_SEQUENCE */
+	/* USER CODE END Boot_Mode_Sequence_2 */
+
+	/* USER CODE BEGIN SysInit */
+
+	/* USER CODE END SysInit */
+
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_TIM2_Init();
+	MX_TIM3_Init();
+	/* USER CODE BEGIN 2 */
+
+	BSP_PB_Init(BUTTON_USER, BUTTON_MODE_GPIO);
+
+	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
+
+	__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, 0);
+	__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 0);
+	counter = DEGREES(__HAL_TIM_GET_COUNTER(&htim3));
+
+	/* USER CODE END 2 */
+
+	/* Initialize leds */
+	BSP_LED_Init(LED_GREEN);
+	BSP_LED_Init(LED_YELLOW);
+	BSP_LED_Init(LED_RED);
+
+	/* Initialize USER push-button, will be used to trigger an interrupt each time it's pressed.*/
+	BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
+
+	/* Initialize COM1 port (115200, 8 bits (7-bit data + 1 stop bit), no parity */
+	BspCOMInit.BaudRate = 115200;
+	BspCOMInit.WordLength = COM_WORDLENGTH_8B;
+	BspCOMInit.StopBits = COM_STOPBITS_1;
+	BspCOMInit.Parity = COM_PARITY_NONE;
+	BspCOMInit.HwFlowCtl = COM_HWCONTROL_NONE;
+	if (BSP_COM_Init(COM1, &BspCOMInit) != BSP_ERROR_NONE) {
+		Error_Handler();
+	}
+
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1) {
+
+		/* USER CODE END WHILE */
+
+		/* USER CODE BEGIN 3 */
+
+		//HAL_Delay(5);
+		counter = DEGREES(__HAL_TIM_GET_COUNTER(&htim3));
+
+		if (BSP_PB_GetState(BUTTON_USER) != GPIO_PIN_RESET) {
+			HAL_Delay(25);
+			if (BSP_PB_GetState(BUTTON_USER) != GPIO_PIN_RESET) {
+				__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, 0);
+				__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 300);
+				while (BSP_PB_GetState(BUTTON_USER) != GPIO_PIN_RESET) {
+					counter = DEGREES(__HAL_TIM_GET_COUNTER(&htim3));
+					HAL_Delay(25);
+				}
+			} else
+				;
+
+		}
+		// PID MODE
+		counter = DEGREES(__HAL_TIM_GET_COUNTER(&htim3));
+
+		if ((counter < 2) || (counter > 357)) {
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, 0);
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 0);
+		}
+
+		counter = DEGREES(__HAL_TIM_GET_COUNTER(&htim3));
+
+		if (counter >= 180) {
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, 200);
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 0);
+
+		} else {
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, 0);
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, 200);
+
+		}
+	}
+		/* USER CODE END 3 */
+	}
+
+	/**
+	 * @brief System Clock Configuration
+	 * @retval None
+	 */
+	void SystemClock_Config(void) {
+		RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+		RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+
+		/** Supply configuration update enable
+		 */
+		HAL_PWREx_ConfigSupply(PWR_DIRECT_SMPS_SUPPLY);
+
+		/** Configure the main internal regulator output voltage
+		 */
+		__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+		while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {
+		}
+
+		/** Initializes the RCC Oscillators according to the specified parameters
+		 * in the RCC_OscInitTypeDef structure.
+		 */
+		RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+		RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
+		RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+		RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+		if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+			Error_Handler();
+		}
+
+		/** Initializes the CPU, AHB and APB buses clocks
+		 */
+		RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+				| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2
+				| RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1;
+		RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+		RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+		RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV1;
+		RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV1;
+		RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV1;
+		RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV1;
+		RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV1;
+
+		if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1)
+				!= HAL_OK) {
+			Error_Handler();
+		}
+	}
+
+	/**
+	 * @brief TIM2 Initialization Function
+	 * @param None
+	 * @retval None
+	 */
+	static void MX_TIM2_Init(void) {
+
+		/* USER CODE BEGIN TIM2_Init 0 */
+
+		/* USER CODE END TIM2_Init 0 */
+
+		TIM_MasterConfigTypeDef sMasterConfig = { 0 };
+		TIM_OC_InitTypeDef sConfigOC = { 0 };
+
+		/* USER CODE BEGIN TIM2_Init 1 */
+
+		/* USER CODE END TIM2_Init 1 */
+		htim2.Instance = TIM2;
+		htim2.Init.Prescaler = 0;
+		htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+		htim2.Init.Period = 1023;
+		htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+		htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+		if (HAL_TIM_PWM_Init(&htim2) != HAL_OK) {
+			Error_Handler();
+		}
+		sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+		sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+		if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig)
+				!= HAL_OK) {
+			Error_Handler();
+		}
+		sConfigOC.OCMode = TIM_OCMODE_PWM1;
+		sConfigOC.Pulse = 0;
+		sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+		sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+		if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1)
+				!= HAL_OK) {
+			Error_Handler();
+		}
+		if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_2)
+				!= HAL_OK) {
+			Error_Handler();
+		}
+		/* USER CODE BEGIN TIM2_Init 2 */
+
+		/* USER CODE END TIM2_Init 2 */
+		HAL_TIM_MspPostInit(&htim2);
+
+	}
+
+	/**
+	 * @brief TIM3 Initialization Function
+	 * @param None
+	 * @retval None
+	 */
+	static void MX_TIM3_Init(void) {
+
+		/* USER CODE BEGIN TIM3_Init 0 */
+
+		/* USER CODE END TIM3_Init 0 */
+
+		TIM_Encoder_InitTypeDef sConfig = { 0 };
+		TIM_MasterConfigTypeDef sMasterConfig = { 0 };
+
+		/* USER CODE BEGIN TIM3_Init 1 */
+
+		/* USER CODE END TIM3_Init 1 */
+		htim3.Instance = TIM3;
+		htim3.Init.Prescaler = 0;
+		htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+		htim3.Init.Period = 65535;
+		htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+		htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+		sConfig.EncoderMode = TIM_ENCODERMODE_TI12;
+		sConfig.IC1Polarity = TIM_ICPOLARITY_RISING;
+		sConfig.IC1Selection = TIM_ICSELECTION_DIRECTTI;
+		sConfig.IC1Prescaler = TIM_ICPSC_DIV1;
+		sConfig.IC1Filter = 0;
+		sConfig.IC2Polarity = TIM_ICPOLARITY_RISING;
+		sConfig.IC2Selection = TIM_ICSELECTION_DIRECTTI;
+		sConfig.IC2Prescaler = TIM_ICPSC_DIV1;
+		sConfig.IC2Filter = 0;
+		if (HAL_TIM_Encoder_Init(&htim3, &sConfig) != HAL_OK) {
+			Error_Handler();
+		}
+		sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+		sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+		if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig)
+				!= HAL_OK) {
+			Error_Handler();
+		}
+		/* USER CODE BEGIN TIM3_Init 2 */
+		HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_ALL);
+		/* USER CODE END TIM3_Init 2 */
+
+	}
+
+	/**
+	 * @brief GPIO Initialization Function
+	 * @param None
+	 * @retval None
+	 */
+	static void MX_GPIO_Init(void) {
+		GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+		/* USER CODE BEGIN MX_GPIO_Init_1 */
+
+		/* USER CODE END MX_GPIO_Init_1 */
+
+		/* GPIO Ports Clock Enable */
+		__HAL_RCC_GPIOC_CLK_ENABLE();
+		__HAL_RCC_GPIOA_CLK_ENABLE();
+		__HAL_RCC_GPIOB_CLK_ENABLE();
+		__HAL_RCC_GPIOG_CLK_ENABLE();
+
+		/*Configure GPIO pins : PC1 PC4 PC5 */
+		GPIO_InitStruct.Pin = GPIO_PIN_1 | GPIO_PIN_4 | GPIO_PIN_5;
+		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+		GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+		HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+		/*Configure GPIO pins : PA1 PA2 PA7 */
+		GPIO_InitStruct.Pin = GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_7;
+		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+		GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+		/*Configure GPIO pin : PB13 */
+		GPIO_InitStruct.Pin = GPIO_PIN_13;
+		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+		GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+		/*Configure GPIO pins : PA8 PA11 PA12 */
+		GPIO_InitStruct.Pin = GPIO_PIN_8 | GPIO_PIN_11 | GPIO_PIN_12;
+		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+		GPIO_InitStruct.Alternate = GPIO_AF10_OTG1_FS;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+		/*Configure GPIO pins : PG11 PG13 */
+		GPIO_InitStruct.Pin = GPIO_PIN_11 | GPIO_PIN_13;
+		GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+		GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+		HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+		/* USER CODE BEGIN MX_GPIO_Init_2 */
+
+		/* USER CODE END MX_GPIO_Init_2 */
+	}
+
+	/* USER CODE BEGIN 4 */
+
+	/* USER CODE END 4 */
+
+	/**
+	 * @brief  This function is executed in case of error occurrence.
+	 * @retval None
+	 */
+	void Error_Handler(void) {
+		/* USER CODE BEGIN Error_Handler_Debug */
+		/* User can add his own implementation to report the HAL error return state */
+		__disable_irq();
+		while (1) {
+		}
+		/* USER CODE END Error_Handler_Debug */
+	}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
